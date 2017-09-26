6339cc207b6e693c596b89015bdfb9ca0fa8a87d
Antti Virtanen <antti.virtanen@solita.fi>
2017-09-26 11:29:45 +0300

    some puzzles to solve

---
+++ README.md
@@ -0,0 +1,15 @@
+# Vulnerable app
+
+One more target for security practice. Nothing fancy here.
+
+## Run
+
+1. Install Python + Flask (```pip install flask```)
+2. ```export FLASK_APP=flask-vuln.py```
+3. ```flask run```
+
+Enjoy the puzzles at ```http://localhost:5000```
+
+# License
+
+See [LICENSE](LICENSE)
---
+++ flask-vuln.py
@@ -0,0 +1,57 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+from flask import Flask, request
+import re
+
+app = Flask(__name__)
+
+@app.route("/")
+def xss():
+    return """<html><body>Hark mortal!<br>
+            This is a simple XSS workshop. In each case, the vulnerable parameter is <i><b>name</b></i><br>
+            <script>
+            var msg = "Hello Brave Hacker! Your mission, should you choose to accept it, is to find and exploit XSS vulnerabilities in this application. The links to challenges are provided here." + 
+            "As always, should you or any of your Hacking Force be caught or killed, the Instructor will disavow any knowledge of your actions. This HTML document will not self-destruct in five/ten seconds. Good luck, Brave Hacker.";
+            
+            if (typeof(window.SpeechSynthesisUtterance) == 'undefined') {
+              alert(msg)
+            } else {
+              var speecher = new SpeechSynthesisUtterance(msg);
+              window.speechSynthesis.speak(speecher);
+            }
+            </script>
+            <hr> 
+            Do <i>alert(0)</i> to win, unless specified otherwise.
+            <ul>
+              <li><a href="/xss1?name=Thor">Level 1: Helppoa kuin heinänteko!</a></li>
+              <li><a href="/xss2?name=Freyja">Level 2: Vasen käsi selän takana..</a></li>
+              <li><a href="/xss3?name=Frigg">Level 3: Ei vieläkään vaikeaa..</a></li>
+            </ul>
+            </body></html>
+           """
+  
+@app.route("/xss1")
+def xss1():
+  f = '<html><body>Mighty ' + request.args.get('name') + ', compose your email now:'
+  g = """
+           <form>To: <input type='text'></input><br>
+           Subject: <input type='text'></input><br>
+           Content: <textarea></textarea><br>
+           <input type="button" value="Send!"/>
+           </form></body></html>
+         """
+  return f + g
+
+@app.route("/xss2")
+def xss2():
+  with open('xss2.html', 'r') as myfile:
+    data=myfile.read().replace('\n', '')
+  content=re.sub('\$name', request.args.get('name'), data)
+  return content
+
+@app.route("/xss3")
+def xss3():
+  with open('xss3.html', 'r') as myfile:
+    data=myfile.read().replace('\n', '')
+  content=re.sub('\$name', request.args.get('name'), data)
+  return content
