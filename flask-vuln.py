071092f856de917f2a5683731d683b91b0d5dcc2
Antti Virtanen <antti.virtanen@solita.fi>
2017-09-27 10:50:03 +0300

    tricky xss

--- flask-vuln.py
+++ flask-vuln.py
@@ -28,12 +28,12 @@ def xss():
               <li><a href="/xss2?name=Freyja">Level 2: Vasen käsi selän takana..</a></li>
               <li><a href="/xss3?name=Frigg">Level 3: Ei vieläkään vaikeaa..</a></li>
               <li><a href="/xss4?name=Odin">Level 4: Joko hikoiluttaa?</a></li>
+              <li><a href="/xss5?name=Loki">Level 5: Loki the base master of deception</a></li>
               <li><a href="/mystery?name=http://localhost:5000">Mystery level</a></li>
               <!-- <li><a href="/bonus?name=xss2.html">Hidden bonus level. <b>No alert(0)!</b>. Instead look for the deepest secret.</a></li> -->
             </ul>
             </body></html>
            """
-
 
 # Notice: this may be extremely dangerous if you are running this on your own computer.
 @app.route("/bonus")
@@ -44,9 +44,23 @@ def bonus():
     data=myfile.read().replace('\n', '')
   return data
 
-@app.route("/mystery")
-def mystery():
-  return redirect(request.args.get('name'), code=302)
+def template(fname):
+  name=request.args.get('name','')
+  with open(fname, 'r') as myfile:
+    data=myfile.read().replace('\n', '')
+  content=re.sub('\$name', name, data)
+  return content
+
+@app.route("/xss5")
+def xss5():
+  return template('xss5.html')
+
+@app.route("/myson")
+def myson():
+  jsonni = request.args.get('name')
+  jsonni = re.sub('[":{},]','',jsonni)
+  return '{"name": "' + jsonni + '"}'
+
 
 @app.route("/xss1")
 def xss1():
@@ -59,13 +73,6 @@ def xss1():
            </form></body></html>
          """
   return f + g
-
-def template(fname):
-  name=request.args.get('name','')
-  with open(fname, 'r') as myfile:
-    data=myfile.read().replace('\n', '')
-  content=re.sub('\$name', name, data)
-  return content
 
 @app.route("/xss2")
 def xss2():
@@ -82,3 +89,8 @@ def xss4():
 @app.route("/deepest-secret")
 def innermystery():
   return template('innermystery.txt')
+
+
+@app.route("/mystery")
+def mystery():
+  return redirect(request.args.get('name'), code=302)
